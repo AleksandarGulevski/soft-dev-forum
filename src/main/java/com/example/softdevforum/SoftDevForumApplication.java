d052a2ce6f287a154d6664e61dcfddd4fbcfb169
Aleksandar Gulevski <agulevski10@gmail.com>
2019-10-04 15:42:31 +0200

    Created yml profiles

---
+++ src/main/java/com/example/softdevforum/SoftDevForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.softdevforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDevForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDevForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softdevforum/SoftDevForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.softdevforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftDevForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
