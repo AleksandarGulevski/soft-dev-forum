6addf732f5e2a05faa895e86a5c8b3010bbe51c3
Aleksandar Gulevski <agulevski10@gmail.com>
2019-10-07 14:36:14 +0200

    Created User entity

--- src/main/java/com/example/softdevforum/entity/User.java
+++ src/main/java/com/example/softdevforum/entity/User.java
@@ -1,4 +1,83 @@
 package com.example.softdevforum.entity;
 
-public class User {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+
+    @OneToMany (
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Post> posts = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
+
+    public void addPost(Post post){
+        posts.add(post);
+        post.setUser(this);
+    }
+
+    public void removePost(Post post){
+        posts.remove(post);
+        post.setUser(null);
+    }
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+        comment.setUser(this);
+    }
+
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+        comment.setUser(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
