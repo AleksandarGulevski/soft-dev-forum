9b24e1cde8ebd3e7a385364b32be92ebacdfcfe0
Aleksandar Gulevski <agulevski10@gmail.com>
2019-10-16 10:34:41 +0200

    Created post API

--- src/main/java/com/example/softdevforum/api/PostController.java
+++ src/main/java/com/example/softdevforum/api/PostController.java
@@ -1,11 +1,48 @@
 package com.example.softdevforum.api;
 
+import com.example.softdevforum.dto.PostDto;
+import com.example.softdevforum.dto.UpdatePostDetailsDto;
+import com.example.softdevforum.entity.Post;
+import com.example.softdevforum.mapper.PostMapper;
+import com.example.softdevforum.service.PostService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("(\"/api/v1/category/posts")
+@RequestMapping("(\"/api/v1")
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping(value = "/{user}/new/post/{category}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public PostDto create(@RequestBody PostDto postDto, @PathVariable ("user") long user, @PathVariable("id") long category) {
+        return PostMapper.entityToDto(postService.create(postDto, user, category));
+    }
+
+    @DeleteMapping(value = "/category/posts/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void delete(@PathVariable("id") long id) {
+        postService.delete(id);
+    }
+
+    @PutMapping(value = "/category/posts/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Post update(@RequestBody UpdatePostDetailsDto updatePostDetailsDto, @PathVariable("id") long id) {
+        return postService.update(updatePostDetailsDto, id);
+    }
+
+    @GetMapping(value = "/category/posts", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<PostDto> getAllUsers() {
+        return postService.getAll().stream()
+                .map(item -> PostMapper.entityToDto(item))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/category/posts/{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Post getPostById(@PathVariable("id") long id) {
+        return postService.getPostById(id);
+    }
 }
